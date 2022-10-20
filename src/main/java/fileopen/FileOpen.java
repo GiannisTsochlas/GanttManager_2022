f26e2bd3f6c38a5a6ff4ccb4feb5efd24d6cc27f
giannisgnnk <cs04970@uoi.gr>
2022-10-20 13:58:56 +0300

    Eftiaksa thn FileOpen kai Task 100%

--- src/main/java/fileopen/FileOpen.java
+++ src/main/java/fileopen/FileOpen.java
@@ -16,7 +16,7 @@ public class FileOpen {
 	}
 	public ArrayList<Task> loadfile(String delimiter, String fileName) {
 		Scanner inputReader = null;
-		{
+		
 		try{
 			inputReader = new Scanner(new FileInputStream(fileName));
 		}
@@ -24,11 +24,22 @@ public class FileOpen {
 			System.out.println("File filename was not found");
 			System.out.println("or could not be opened");
 			System.exit(0);
-			}
-		
-		String line = inputReader.nextLine();
-		
-		inputReader.close();
 		}
+		ArrayList<Task> task = new ArrayList<Task>();
+		while(inputReader.hasNextLine()) {
+				String line = inputReader.nextLine();
+				String[] words = line.split(delimiter);
+				for(int i=0; i<words.length; i++) {
+					if(words.length==3) {
+						Task task1 = new Task(Integer.parseInt(words[0]),words[1],Integer.parseInt(words[2]));
+						task.add(task1);
+					}else if(words.length==6) {
+						Task task1 = new Task(Integer.parseInt(words[0]),words[1],Integer.parseInt(words[2]),Integer.parseInt(words[3]),Integer.parseInt(words[4]),Integer.parseInt(words[5]));
+						task.add(task1);
+					}
+				}
+		}
+		inputReader.close();
+		return task;	
 	}
 }
--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -1,25 +1,52 @@
 package task;
 
 public class Task {
-	int taskId;
-	int mamaId;
-	int start;
-	int end;
-	int cost;
+	private int taskId;
+	private String name;
+	private int mamaId;
+	private int start;
+	private int end;
+	private int cost;
 	
-	public Task(int taskId, int mamaId, int start, int end, int cost) {
+	public Task(int taskId, String name, int mamaId, int start, int end, int cost) {
 		this.taskId=taskId;
+		this.name=name;
 		this.mamaId=mamaId;
 		this.start=start;
 		this.end=end;
 		this.cost=cost;
 	}
 	
-	public Task(int taskId, int mamaId) {
+	public Task(int taskId, String name, int mamaId) {
 		this.taskId=taskId;
+		this.name=name;
 		this.mamaId=0;
 		this.start=0;
 		this.end=0;
 		this.cost=0;
-	}	
+	}
+	
+	public int getTaskId() {
+		return taskId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getMamaId() {
+		return mamaId;
+	}
+	
+	public int getStart() {
+		return start;
+	}
+	
+	public int getEnd() {
+		return end;
+	}
+	
+	public int getCost() {
+		return cost;
+	}
 }

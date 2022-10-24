37ea4775504760e3b20a4080ab2d1e8b37281198
GitHub_GiannisTsxolas <cs04993@uoi.gr>
2022-10-24 16:38:45 +0300

    completion to reports

--- src/main/java/reports/TextReport.java
+++ src/main/java/reports/TextReport.java
@@ -2,7 +2,11 @@ package reports;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import backend.ReportType;
 
 
@@ -15,7 +19,26 @@ public class TextReport {
 		this.path = path;
 		this.type = type;
 	}
+	
+	 public static int countLineJava(String fileName) {
 
+	      Path path = Paths.get(fileName);
+
+	      int lines = 0;
+	      try {
+
+	          // much slower, this task better with sequence access
+	          //lines = Files.lines(path).parallel().count();
+
+	          lines = (int) Files.lines(path).count();
+
+	      } catch (IOException e) {
+	          e.printStackTrace();
+	      }
+
+	      return lines;
+
+	  }
 
 
 	public int createReportTxt(String path, ReportType type) {
@@ -27,14 +50,13 @@ public class TextReport {
 		}
 		catch (FileNotFoundException e){
 		System.out.println("Error opening the file stuff.txt.");
-		System.exit(0);
+		return -1;
 		}
 		
 		PrintWriter outputWriter = new PrintWriter(outputStream);
 		
 		outputWriter.println("jumped over the lazy dog.");
 		outputWriter.close( );
-		
-		return 0;
+		return countLineJava(path);
 	}
 }
